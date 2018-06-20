0c437f3b449f5d23a3e7e7c928d5f42fcbf53d68
limingfeng <limingfeng@xinzhentech.com>
2018-06-20 15:22:39 +0800

    修改

--- manage/src/main/java/com/xinzhentech/study/manage/controller/product/ProductController.java
+++ manage/src/main/java/com/xinzhentech/study/manage/controller/product/ProductController.java
@@ -1,4 +1,19 @@
 package com.xinzhentech.study.manage.controller.product;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @author limingfeng@xinzhentech.com
+* @since 2018/6/20 15:21
+*
+* @desc 产品相关接口
+*/
+@RestController
+@Slf4j
+@RequestMapping(value = "/product")
+@Api(description = "产品相关信息")
 public class ProductController {
 }
