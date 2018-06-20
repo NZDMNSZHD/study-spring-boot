3de468386ffd60710d2084840e5288f3f194ef24
limingfeng <limingfeng@xinzhentech.com>
2018-06-20 15:29:13 +0800

    添加项目相关接口

--- manage/src/main/java/com/xinzhentech/study/manage/controller/project/ProjectController.java
+++ manage/src/main/java/com/xinzhentech/study/manage/controller/project/ProjectController.java
@@ -1,4 +1,19 @@
 package com.xinzhentech.study.manage.controller.project;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @author limingfeng@xinzhentech.com
+* @since 2018/6/20 15:27
+*
+* @desc 项目相关接口
+*/
+@RestController
+@Slf4j
+@RequestMapping(value = "/project")
+@Api(description = "项目相关信息")
 public class ProjectController {
 }
